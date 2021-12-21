fab84f9945929af4975a9407556d35c4242e477b
devriazul <engr.riazul@gmail.com>
2021-12-21 19:10:33 +0600

    Last update

---
+++ app/Question.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute(){
+
+        return route('questions.show',$this->slug);
+    }
+
+    public function getCreatDateAttribute(){
+
+        return $this->created_at->diffForHuman();
+    }
+
+    public function getStatusAttribute()
+    {
+        if($this->answers>0){
+            if($this->best_answer_id){
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+
+}
---
+++ resources/views/question/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center"|>
+                            <h2>Ask Question</h2>
+                            <div class="ml-auto"|>
+                                   <a class="btn btn-outline-secondary" href="{{route('questions.index')}}"> Back to all Question </a>
+                            </div>
+                    </div>
+                    
+                </div>
+
+                <div class="card-body">
+                    <form action="{{route('questions.store')}}" method="post">
+                      @include('question._form', ['buttonText' => 'Ask Question'])
+                    </form>
+                   
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
